cf317a42a47409e2d860875617f7c274df2393b5
Julian <nortonjulian@gmail.com>
2023-06-06 14:51:57 -0600

    next commit

--- src/Carousel.test.js
+++ src/Carousel.test.js
@@ -2,6 +2,15 @@ import React from "react";
 import { render, fireEvent } from "@testing-library/react";
 import Carousel from "./Carousel";
 
+it("renders without crashing", function() {
+  render(<Carousel />);
+});
+
+it("matches snapshot", function() {
+  const { asFragment } = render(<Carousel />);
+  expect(asFragment()).toMatchSnapshot();
+});
+
 it("works when you click on the right arrow", function() {
   const { queryByTestId, queryByAltText } = render(<Carousel />);
 
@@ -16,4 +25,37 @@ it("works when you click on the right arrow", function() {
   // expect the second image to show, but not the first
   expect(queryByAltText("Photo by Richard Pasquarella on Unsplash")).not.toBeInTheDocument();
   expect(queryByAltText("Photo by Pratik Patel on Unsplash")).toBeInTheDocument();
+});
+
+it("hides and shows arrows", function () {
+  const { getByTestId } = render(<Carousel />);
+  const leftArrow = getByTestId("left-arrow")
+  const rightArrow = getByTestId("right-arrow")
+
+  expect(leftArrow).toHaveClass("hidden");
+  expect(rightArrow).not.toHaveClass("hidden");
+
+  fireEvent.click(getByTestId("right-arrow"));
+
+  expect(leftArrow).not.toHaveClass("hidden");
+  expect(rightArrow).not.toHaveClass("hidden");
+
+  fireEvent.click(rightArrow);
+ expect(leftArrow).not.toHaveClass("hidden");
+ expect(rightArrow).toHaveClass("hidden");
+
+})
+
+it("works when you click on the left arrow", function() {
+  const { getByTestId, queryByAltText } = render(<Carousel />);
+  const leftArrow = getByTestId("left-arrow");
+  const rightArrow = getByTestId("right-arrow");
+
+  // move to the right
+  fireEvent.click(rightArrow);
+
+  // move back to the left, expect the first image to show
+  fireEvent.click(leftArrow);
+  expect(queryByAltText("Photo by Richard Pasquarella on Unsplash")).toBeInTheDocument();
+  expect(queryByAltText("Photo by Pratik Patel on Unsplash")).not.toBeInTheDocument();
 });
